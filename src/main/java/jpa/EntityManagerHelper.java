7558266c0bfbafdacd35780f27aad33bbcce084c
kaouthar <kaou.bennouna@gmail.com>
2016-02-18 02:39:12 +0100

    Debut q5

---
+++ src/main/java/jpa/EntityManagerHelper.java
@@ -0,0 +1,54 @@
+package jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EntityManagerHelper {
+	  private static final EntityManagerFactory factory; 
+	    private static final ThreadLocal<EntityManager> threadLocal;
+
+	    static {
+	        factory = Persistence.createEntityManagerFactory("mysql");
+	        threadLocal = new ThreadLocal<EntityManager>();
+	    }
+
+	    public static EntityManager getEntityManager() {
+	        EntityManager em = threadLocal.get();
+
+	        if (em == null) {
+	            em = factory.createEntityManager();
+	            threadLocal.set(em);
+	        }
+	        return em;
+	    }
+
+	    public static EntityManagerFactory getfactory() {
+			return factory;
+		}
+
+		public static void closeEntityManager() {
+	        EntityManager em = threadLocal.get();
+	        if (em != null) {
+	            em.close();
+	            threadLocal.set(null);
+	        }
+	    }
+
+	    public static void closeEntityManagerFactory() {
+	        factory.close();
+	    }
+
+	    public static void beginTransaction() {
+	        getEntityManager().getTransaction().begin();
+	    }
+
+	    public static void rollback() {
+	        getEntityManager().getTransaction().rollback();
+	    }
+
+	    public static void commit() {
+	        getEntityManager().getTransaction().commit();
+	    } 
+
+}
---
+++ src/main/java/servlet/Opower.java
@@ -0,0 +1,55 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.persistence.EntityTransaction;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import domain.Heaters;
+import domain.Home;
+import jpa.EntityManagerHelper;
+
+@WebServlet(name="opower",
+urlPatterns={"/Opower"})
+public class Opower extends HttpServlet {
+	public void doPost(HttpServletRequest request,
+			HttpServletResponse response)
+throws ServletException, IOException {
+response.setContentType("text/html");
+
+PrintWriter out = response.getWriter();
+
+
+out.println("<HTML>\n<BODY>\n" +
+		"<H1>Affichage des informations</H1>\n" +
+		"<UL>\n" +			
+" <LI>Nom: "
+		+ request.getParameter("nom") + "\n" +
+		" <LI>Prenom: "
+		+ request.getParameter("prenom") + "\n" +
+		" <LI>Email: "
+		+ request.getParameter("email") + "\n" +
+		"</UL>\n" +				
+"</BODY></HTML>");
+
+	
+	EntityTransaction tx = EntityManagerHelper.getEntityManager().getTransaction();
+	tx.begin();
+	try {
+		Home home0 = new Home(3,100);
+		Heaters heater6=new Heaters(200, home0);
+		EntityManagerHelper.getEntityManager().persist(home0);
+		EntityManagerHelper.getEntityManager().persist(heater6);
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	tx.commit();
+
+	out.println("Enregistrement effectué</BODY></HTML>");
+}
+}
