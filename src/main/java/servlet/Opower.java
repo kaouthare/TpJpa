084ca828bd260ff7140ce9fcf31e57458bc7b0b8
kaouthar <kaou.bennouna@gmail.com>
2016-04-19 17:39:04 +0200

    Opower.java changement pour peulpler la base question 5

--- src/main/java/servlet/Opower.java
+++ src/main/java/servlet/Opower.java
@@ -12,9 +12,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import domain.ElectronicDevices;
 import domain.Heaters;
 import domain.Home;
 import domain.Person;
+import domain.SmartDevices;
 import jpa.EntityManagerHelper;
 
 @WebServlet(name="opower",
@@ -46,13 +48,28 @@ out.println("<HTML>\n<BODY>\n" +
 	EntityTransaction tx = EntityManagerHelper.getEntityManager().getTransaction();
 	tx.begin();
 	try {
-		Home home0 = new Home(3,100);
-		Heaters heater6=new Heaters(200, home0);
+		Home home0 = new Home("residence principale",300,10);
+		Heaters C1 = new Heaters("chauffage cuisine", 150, home0);
+		Heaters C2 = new Heaters("chauffage salon", 200, home0);
+		ElectronicDevices Ed1= new ElectronicDevices("Tv", 20, home0);
+		ElectronicDevices Ed2= new ElectronicDevices("Tel", 50, home0);
+		
+		List<SmartDevices> listConsoChauffage = new ArrayList<SmartDevices>();
+		listConsoChauffage.add(C1);
+		listConsoChauffage.add(C2);
+		List<SmartDevices> listConsoED = new ArrayList<SmartDevices>();
+		listConsoED.add(Ed1);
+		listConsoED.add(Ed2);
+		home0.setElectronicDevices(listConsoED);
+		home0.setHeaters(listConsoChauffage);
 		List<Home> homes = new ArrayList<Home>();
 		homes.add(home0);
 		Person personne = new Person(request.getParameter("nom"), request.getParameter("prenom"), request.getParameter("email"), homes);
 		EntityManagerHelper.getEntityManager().persist(home0);
-		EntityManagerHelper.getEntityManager().persist(heater6);
+		EntityManagerHelper.getEntityManager().persist(C1);
+		EntityManagerHelper.getEntityManager().persist(C2);
+		EntityManagerHelper.getEntityManager().persist(Ed1);
+		EntityManagerHelper.getEntityManager().persist(Ed2);
 		EntityManagerHelper.getEntityManager().persist(personne);
 	} catch (Exception e) {
 		e.printStackTrace();
